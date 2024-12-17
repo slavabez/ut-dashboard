2b6a198e34bc0e373eceb7370bd449ee7643da94
Slava Bezgachev <bezgachev@gmail.com>
2024-12-17 17:43:36 +0500

    chore:  disabled sentry for now

--- next.config.mjs
+++ next.config.mjs
@@ -37,7 +37,9 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
+export default nextConfig;
+
+withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
@@ -70,10 +72,4 @@ export default withSentryConfig(nextConfig, {
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
 });
